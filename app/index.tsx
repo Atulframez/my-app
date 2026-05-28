021bd55361f1fc54028fb37e667ebabc8fa6c8e6
Atul Anand <atulanand092004@gmail.com>
2026-05-28 23:25:57 +0530

    update text style in Index component for improved readability

--- app/index.tsx
+++ app/index.tsx
@@ -10,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold", color: "white", textAlign: "center", 
+        fontStyle: "italic" }}>
         Welcome to the Atul-First-App
       </Text>
     </View>
