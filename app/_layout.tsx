84b78cbf500f02b4ba8fd2f1fb1de52712399731
Atul Anand <atulanand092004@gmail.com>
2026-05-30 01:14:00 +0530

    refactor RootLayout component to include screen options for improved navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: 'red' },
+        headerTintColor: 'white',
+        headerTitleStyle: { fontWeight: 'bold' },
+      }}
+    >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="about" />
+    </Stack>
+  );
+};
+
+export default RootLayout;
