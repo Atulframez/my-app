93dfb0ee868c4ecf6c9a7b09c601d874c0ead49c
Atul Anand <atulanand092004@gmail.com>
2026-05-30 12:54:47 +0530

    add drawer navigation to RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { Stack } from 'expo-router';
 const RootLayout = () => {
   return (
     <Stack>
+        <Stack.Screen name="(drawer)" />
         <Stack.Screen name="(tab)"  options={{ headerShown: false }} />
         <Stack.Screen name="index" />
         <Stack.Screen name="about" />
